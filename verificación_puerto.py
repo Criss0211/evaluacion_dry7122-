96672f227f96c1b8deb68e32c27f0d14ac32c643
NetAcad DEVASC <changeme@example.com>
2025-04-18 17:24:03 +0000

    C

--- verificación_puerto.py
+++ verificación_puerto.py
@@ -1,7 +1,7 @@
 try:
 
     puerto = int(input("Introduce el número de puerto: "))
-    
+
     if 0 <= puerto <= 1023:
         print(f"El puerto {puerto} es un Puerto bien conocido.")
     elif 1024 <= puerto <= 49151:
@@ -12,4 +12,4 @@ try:
         print("El número ingresado no corresponde a un puerto válido (debe estar entre 0 y 65535).")
 
 except ValueError:
-    print("Entrada inválida. Debe ingresar un número entero.")
+    print("Entrada inválida. Debe ingresar un número entero.") 
