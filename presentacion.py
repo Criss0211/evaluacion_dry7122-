e234291b38b6f0400d001969d6477ea25d94166c
NetAcad DEVASC <changeme@example.com>
2025-04-18 17:21:42 +0000

    A

---
+++ presentacion.py
@@ -0,0 +1,11 @@
+print("Evaluación N°1 Programación y Redes Virtualizadas\n")
+
+
+participantes = [
+    "Sebastian Vega",
+    "Cristhofer Gutierrez"
+]
+
+print("Participantes de la evaluación:")
+for nombre in participantes:
+    print(f"- {nombre}")
